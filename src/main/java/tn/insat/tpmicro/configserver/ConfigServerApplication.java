685885fe9ba3796630e0056b3021a773f1a6583a
Wassim Benzarti <wass11121996@gmail.com>
2020-04-27 14:10:03 +0000

    Made

--- src/main/java/tn/insat/tpmicro/configserver/ConfigServerApplication.java
+++ src/main/java/tn/insat/tpmicro/configserver/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package tn.insat.tpmicro.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
